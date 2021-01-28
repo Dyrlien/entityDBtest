d5918a6035db1427c87e70bc5dec5bb981d88ee3
Dyrlien <46710693+Dyrlien@users.noreply.github.com>
2021-01-28 11:56:32 +0100

    Add project files.

---
+++ entityDBtest/Migrations/20210127174154_CreateCurrencyDB.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace entityDBtest.Migrations
+{
+    public partial class CreateCurrencyDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Currency",
+                columns: table => new
+                {
+                    Date = table.Column<string>(type: "nvarchar(10)", nullable: false),
+                    CurrencyCode = table.Column<string>(type: "nvarchar(3)", nullable: true),
+                    Rate = table.Column<string>(type: "nvarchar(15)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currency", x => x.Date);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Currency");
+        }
+    }
+}
---
+++ entityDBtest/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace entityDBtest
+{
+    public class CurrencyContext : DbContext
+    {
+        public DbSet<Currency> Currency { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=CurrencyDB;Trusted_Connection=True;");
+        }
+    }
+    public class Currency
+    {
+        [Key]
+        public string Date { get; set; }
+        public string CurrencyCode { get; set; }
+        public string Rate { get; set; }
+
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
